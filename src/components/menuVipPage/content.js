2bc979f60193c9376bca044b623e674f8dfe4bd1
Ka-ui Chu <kaui.chu.tw@gmail.com>
2021-02-23 22:33:55 +0100

    first update for vip page (no css, bad page structure)

--- src/components/menuVipPage/content.js
+++ src/components/menuVipPage/content.js
@@ -7,6 +7,7 @@ export default class Content {
     }
 
     init = () => {
+        this.user = window.Database.DB.users[0] // TODO update the user id
     }
 
     render = () => {
@@ -16,9 +17,32 @@ export default class Content {
     }
 
     createInformationBox = () => {
-        var content = $('<div class="info-page-container">' +
-                            '<p>Menu VIP test</p>'+
-                        '</div>');
+
+        const content = $('<div class="vip-page-container"></div>');
+
+
+        let item = $('<div class="item-inner"></div>');
+        item.append($('<div class="item-name">Password of combination lock</div>'));
+        item.append($('<div class="item-desc">' + this.getPassword() + '</div>'));
+        $(content).append(item);
+
+        item = $('<div class="item-inner"></div>');
+        item.append($('<div class="item-name">account balance</div>'));
+        item.append($('<div class="item-desc">' + 1234 + '</div>'));
+        $(content).append(item);
+
+
+        // var content = $('<div class="info-page-container">' +
+        //                     '<p>Menu VIP test</p>'+
+        //                 '</div>');
+
         return content;
+    }
+
+    getPassword = () => {
+        const d = new Date();
+        const month = d.getMonth()+1;
+        const day = d.getDate();
+        return (month < 10 ? '0' : '') + month + (day < 10 ? '0' : '') + day;
     }
 }
