b55d5a6eda905e446832ebd391f755929669a2a1
rebeckaberglin <rebecka.berglin@gmail.com>
2021-03-13 16:42:38 +0100

    removed comments

--- src/db/OrdersData.js
+++ src/db/OrdersData.js
@@ -2,8 +2,6 @@ export default class OrdersData {
     constructor() {
       this.data = []
       this.idCounter = 0;
-      //remove
-      //this.addOrderFunc();
     }
 
     addOrder(table, items){
@@ -13,15 +11,4 @@ export default class OrdersData {
     this.data.push(order);
     this.idCounter++;
    } 
-
-
-  // //remove 
-  // //test function to add orders
-  // addOrderFunc = () => {
-  //   this.addOrder(4, [{item: "beer", price: "42"}]);
-
-  //   this.addOrder(5, [{item: "drink", price: "89"}]);
-
-  //   this.addOrder(8, [{item: "beer", price: "39"},{item: "wine", price: "49"}]);
-  // }
 }
